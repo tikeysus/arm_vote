a45dc2746e4a2f437473ca40df98c9d80fdfa1ca
Tigran Tumanyan <tigrantumanyann@gmail.com>
2026-02-05 21:11:54 -0500

    file structure for what is to come.

---
+++ target/debug/deps/crypto_core-27aa33e5e0ac53cd.d
@@ -0,0 +1,7 @@
+/Users/tikeysus/Documents/projects/arm_vote/target/debug/deps/crypto_core-27aa33e5e0ac53cd: crypto_core/src/lib.rs crypto_core/src/errors.rs crypto_core/src/modint/mod.rs
+
+/Users/tikeysus/Documents/projects/arm_vote/target/debug/deps/crypto_core-27aa33e5e0ac53cd.d: crypto_core/src/lib.rs crypto_core/src/errors.rs crypto_core/src/modint/mod.rs
+
+crypto_core/src/lib.rs:
+crypto_core/src/errors.rs:
+crypto_core/src/modint/mod.rs:
