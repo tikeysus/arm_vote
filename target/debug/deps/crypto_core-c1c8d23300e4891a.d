c5cddea47040fa031d85bac300479d4c919cd0b6
Tigran Tumanyan <tigrantumanyann@gmail.com>
2026-02-07 14:38:26 -0500

    Stop tracking target/; add to .gitignore

--- target/debug/deps/crypto_core-c1c8d23300e4891a.d
+++
@@ -1,9 +0,0 @@
-/Users/tikeysus/Documents/projects/arm_vote/target/debug/deps/libcrypto_core-c1c8d23300e4891a.rmeta: crypto_core/src/lib.rs crypto_core/src/errors.rs crypto_core/src/modint/mod.rs
-
-/Users/tikeysus/Documents/projects/arm_vote/target/debug/deps/libcrypto_core-c1c8d23300e4891a.rlib: crypto_core/src/lib.rs crypto_core/src/errors.rs crypto_core/src/modint/mod.rs
-
-/Users/tikeysus/Documents/projects/arm_vote/target/debug/deps/crypto_core-c1c8d23300e4891a.d: crypto_core/src/lib.rs crypto_core/src/errors.rs crypto_core/src/modint/mod.rs
-
-crypto_core/src/lib.rs:
-crypto_core/src/errors.rs:
-crypto_core/src/modint/mod.rs:
