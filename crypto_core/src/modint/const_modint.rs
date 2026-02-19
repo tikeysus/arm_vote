645b91e5bf71ed8b57cf680c5a7a7edebb4006f3
Tigran Tumanyan <tigrantumanyann@gmail.com>
2026-02-19 15:47:43 -0500

    propagating errors

--- crypto_core/src/modint/const_modint.rs
+++ crypto_core/src/modint/const_modint.rs
@@ -1,7 +1,7 @@
 use crate::errors::CryptoError; 
 
 pub struct ConstModInt<const P: u64>{
-    value: u64,
+    pub value: u64,
 }
 
 impl<const P: u64> ConstModInt<P>{
@@ -9,36 +9,36 @@ impl<const P: u64> ConstModInt<P>{
 		if P == 0 { 
 			return Err(CryptoError::ModulusIsZero); 
 		}
-		ok(Self { value: x % P }) 
+		Ok(Self { value: x % P }) 
 	}
 
 	pub fn value(self) -> u64{
 		self.value
 	}
 
-	pub fn add(self, other: Self) -> Self{
+	pub fn add(self, other: Self) -> Result<Self, CryptoError>{
 		let result = (self.value + other.value) % P; 
 		Self::new(result)
 	}
 
-	pub fn sub(self, other: Self) -> Self{
+	pub fn sub(self, other: Self) -> Result<Self, CryptoError>{
 		let result = (self.value + P - other.value) % P; 
 		Self::new(result)
 	}
 
-	pub fn mul(self, other: Self) -> Self{
+	pub fn mul(self, other: Self) -> Result<Self, CryptoError>{
 		let result = (self.value * other.value) % P; 
 		Self::new(result) 
 	}
 
-	pub fn pow(self, mut exponent: u64) -> Self{
+	pub fn pow(self, mut exponent: u64) -> Result<Self, CryptoError>{
 		if exponent == 0 { return Self::new(1) }
-		if exponent == 1 { return self }
+		if exponent == 1 { return Ok(self) }
 
 		let mut result = 1; 
 		let mut base = self.value % P; 
-		while (exponent > 0){
-			if (exponent & 1 == 1){
+		while exponent > 0{
+			if exponent & 1 == 1{
 				result = (result * base) % P; 
 			}
 			base = (base * base) % P; 
@@ -47,7 +47,8 @@ impl<const P: u64> ConstModInt<P>{
 		Self::new(result) 
 	}
 
-	pub fn print_modulus() {
-		println!("Modulus is {}", P);
+	pub fn inverse(self) -> Result<Self, CryptoError>{
+		unimplemented!()
 	}
+
 }
